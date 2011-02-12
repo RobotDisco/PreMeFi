721ba73f894e1c24a27ce36e85f838986758dd29
The Lambda Calculus <code@thelambdacalculus.net>
2011-02-12 16:47:31 -0500

    Updating some copyleft headers

--- app/models/MetaFeed.js
+++ app/models/MetaFeed.js
@@ -1,5 +1,5 @@
 //  PreMeFi - A MetaFilter viewer for Palm's WebOS
-//  Copyright (C) 2010  Gaelan D'costa <gdcosta@gmail.com>
+//  Copyright (C) 2011  Gaelan D'costa <gdcosta@gmail.com>
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
